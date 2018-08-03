c861ae6a67a1f105cdaa0df44d9cb1665883db9c
Parker Merritt <parkermerritt@Parkers-MacBook-Pro.local>
2018-08-03 01:11:55 -0400

    Update majority of info and add profile pic

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import Waypoint from 'react-waypoint'
 
 import Header from '../components/Header'
 import Nav from '../components/Nav'
-import pic01 from '../assets/images/pic01.jpg'
+import profile from '../assets/images/EricProfile.jpg'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -56,23 +56,16 @@ class Index extends React.Component {
                   belt. I excel especially at first person shooters and sports
                   games.
                 </p>
-                <ul className="actions">
-                  <li>
-                    <Link to="/generic" className="button">
-                      Learn More
-                    </Link>
-                  </li>
-                </ul>
               </div>
               <span className="image">
-                <img src={pic01} alt="" />
+                <img src={profile} alt="" />
               </span>
             </div>
           </section>
 
           <section id="first" className="main special">
             <header className="major">
-              <h2>My Services</h2>
+              <h2>Why E-Sports?</h2>
               <p>
                 E-sports are the rising star of modern entertainment platforms,
                 and gamers are flocking to monetize their skills through
@@ -82,44 +75,33 @@ class Index extends React.Component {
             </header>
             <ul className="statistics">
               <li className="style1">
-                <span className="icon fa-code-fork" />
-                <strong>5,120</strong> Etiam
+                <span className="icon fa-signal" />
+                <strong>$906</strong>million revenue '18
               </li>
               <li className="style2">
-                <span className="icon fa-folder-open-o" />
-                <strong>8,192</strong> Magna
+                <span className="icon fa-users" />
+                <strong>380</strong>million viewers
               </li>
               <li className="style3">
-                <span className="icon fa-signal" />
-                <strong>2,048</strong> Tempus
+                <span className="icon fa-eye" />
+                <strong>84</strong>mins avg. watch
               </li>
               <li className="style4">
-                <span className="icon fa-laptop" />
-                <strong>4,096</strong> Aliquam
+                <span className="icon fa-arrow-up" />
+                <strong>29%</strong>annual growth
               </li>
               <li className="style5">
                 <span className="icon fa-diamond" />
-                <strong>1,024</strong> Nullam
+                <strong>$1.5B</strong>est. revenue 2021
               </li>
             </ul>
-            <p className="content">
-              Nam elementum nisl et mi a commodo porttitor. Morbi sit amet nisl
-              eu arcu faucibus hendrerit vel a risus. Nam a orci mi, elementum
-              ac arcu sit amet, fermentum pellentesque et purus. Integer maximus
-              varius lorem, sed convallis diam accumsan sed. Etiam porttitor
-              placerat sapien, sed eleifend a enim pulvinar faucibus semper quis
-              ut arcu. Ut non nisl a mollis est efficitur vestibulum. Integer
-              eget purus nec nulla mattis et accumsan ut magna libero. Morbi
-              auctor iaculis porttitor. Sed ut magna ac risus et hendrerit
-              scelerisque. Praesent eleifend lacus in lectus aliquam porta. Cras
-              eu ornare dui curabitur lacinia.
-            </p>
+
             <footer className="major">
               <ul className="actions">
                 <li>
-                  <Link to="/generic" className="button">
+                  <a href="http://www.espn.com/esports/" className="button">
                     Learn More
-                  </Link>
+                  </a>
                 </li>
               </ul>
             </footer>
